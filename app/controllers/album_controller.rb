e7b7c3a5e002664ae25bc97b16b98a7839c4071f
Jamie Simper <fromraintohope@gmail.com>
2015-07-10 08:21:56 -0400

    setup album controller

--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -1,24 +1,47 @@
 class AlbumController < ApplicationController
 
   def index
+    @albums = Album.all
   end
 
   def new
+    @album = Album.new
   end
 
   def create
+    @album = Album.new(album_params)
+   if @album.save
+     redirect_to albums_path
+   else
+     render :new
+   end
   end
 
   def show
+    @album = Album.find(params[:id])
   end
 
   def edit
+    @album = Album.find(params[:id])
   end
 
   def update
+    @album = Album.find(params[:id])
+    if @album.update_attributes(album_params)
+      redirect_to albums_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @album = Album.find(params[:id]).destroy
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :artist, :genre, :length)
   end
 
 end
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,7 +1,71 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  def setup
 
+  test "create album works" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist', length: 1)
+    assert a.valid?
   end
+
+  test "save album works" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist', length: 1)
+    a.save
+    assert Album.exists?(a.id)
+  end
+
+  test "title must be present" do
+    a = Album.new(genre: 'Test Genre', artist: 'Test Artist', length: 1)
+    refute a.valid?
+    assert a.errors.keys.include?(:title)
+  end
+
+  test "artist must be present" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', length: 1)
+    refute a.valid?
+    assert a.errors.keys.include?(:artist)
+  end
+
+  test "genre must be present" do
+    a = Album.new(title: 'Test Title', artist: 'Test Artist', length: 1)
+    refute a.valid?
+    assert a.errors.keys.include?(:genre)
+  end
+
+  test "length must be present" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist')
+    refute a.valid?
+    assert a.errors.keys.include?(:length)
+  end
+
+  test "title length must be greater than 0" do
+    a = Album.new(title: '', genre: 'Test Genre', artist: 'Test Artist', length: 1)
+    refute a.valid?
+    assert a.errors.keys.include?(:title)
+  end
+
+  test "artist length must be greater than 0" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: '', length: 1)
+    refute a.valid?
+    assert a.errors.keys.include?(:artist)
+  end
+
+  test "genre length must be greater than 0" do
+    a = Album.new(title: 'Test Title', genre: '', artist: 'Test Artist', length: 1)
+    refute a.valid?
+    assert a.errors.keys.include?(:genre)
+  end
+
+  test "length length must be greater than 0" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist', length: 0)
+    refute a.valid?
+    assert a.errors.keys.include?(:length)
+  end
+
+  test "no duplicates of artist and song combo" do
+    a = Album.new(title: 'Test Title', genre: 'Test Genre', artist: 'Test Artist', length: 10)
+    a.save
+    b = Album.new(title: 'Test Title', genre: 'Test_Genre', artist: 'Test Artist', length: 30)
+    refute b.valid?
+  end
+
 end
